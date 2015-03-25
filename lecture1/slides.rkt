d21f0588bf290aa40e5d65a0904c865317c88c16
Jack Firth <jackhfirth@gmail.com>
2015-03-25 15:26:30 -0700

    Add initial slides and helpers

--- lecture1/slides.rkt
+++ lecture1/slides.rkt
@@ -8,7 +8,6 @@
 (define topics void)
 
 (topics
- "pure functions"
  "simple composition with monadic functions"
  "currying as a way of making multi-argument functions composable"
  "data last"
@@ -20,3 +19,31 @@
  "monoids"
  "concat"
  "tryadic functions where first argument often config for concat of some sort")
+
+
+(define (bullet-slide title init-para-text . items)
+  (apply slide
+         #:title title
+         #:layout 'center
+         (para (t init-para-text))
+         (add-nexts (map item items))))
+
+
+(define (add-nexts elements)
+  (if (empty? elements)
+      '()
+      (list* 'next (first elements) (add-nexts (rest elements)))))
+
+
+(bullet-slide "Pure functions"
+              "A pure function must only depend on its arguments"
+              "Must have no internal state"
+              "Must not touch the outside world"
+              "Must do the same thing when called twice")
+
+
+(bullet-slide "So what?"
+              "Why bother making a function pure?"
+              "No dependency on time"
+              "Easy to test"
+              "Easy to compose")
