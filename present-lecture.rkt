0d407f14bf79ecbbd60304670dbc8236ff5263b8
Jack Firth <jackhfirth@gmail.com>
2015-03-25 15:04:44 -0700

    Fix lecture path construction

--- present-lecture.rkt
+++ present-lecture.rkt
@@ -1,15 +1,19 @@
 #lang racket
 
 (require command-line-ext
-         raco/command-name)
+         raco/command-name
+         racket/runtime-path)
 
+
+(define-runtime-path source-dir ".")
 
 (define-syntax-rule (raco-command-line form ...)
   (command-line-ext
    #:program (short-program+command-name)
    form ...))
 
+
 (raco-command-line
  #:args (lecture-name)
- (define lecture-path (format "./~a/main.rkt" lecture-name))
+ (define lecture-path (build-path source-dir lecture-name "main.rkt"))
  (dynamic-require lecture-path #f))
